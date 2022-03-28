e724d15980d5a5e458e24ef9d819bd967aadd266
hilmizul <netspytux@gmail.com>
2022-03-28 13:29:48 +0700

    tes

--- adm/views/project.php
+++ adm/views/project.php
@@ -1,5 +1,33 @@
 <div class="row">
   <div class="col-md-12">
-    <h1>project</h1>
+    <h1>
+      Project
+      <a href="#" class="btn btn-primary">Tambah</a>
+    </h1>
+    <table class="table table-striped" id="">
+      <thead>
+        <tr>
+          <th width="5%">#</th>
+          <th width="15%">FOTO</th>
+          <th>NAMA</th>
+          <th width="20%">AKSI</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $q = mysqli_query($koneksi, "select * from projek");
+        while($data = mysqli_fetch_array($q)) {?>
+        <tr>
+          <td><?=$data['id']?></td>
+          <td><img src="<?=$data['foto']?>" width="130px"></td>
+          <td><?=$data['nama']?></td>
+          <td>
+            <a href="#" class="btn btn-info">Ubah</a>
+            <a href="#" class="btn btn-danger">Hapus</a>
+          </td>
+        </tr>
+        <?php } ?>
+      </tbody>
+    </table>
   </div>
 </div>
