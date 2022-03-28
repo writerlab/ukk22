ebc072e1b52e314f82f11623d350d864535cd70a
hilmizul <netspytux@gmail.com>
2022-03-28 13:40:56 +0700

    form add project

---
+++ adm/views/tambah-project.php
@@ -0,0 +1,20 @@
+<div class="row">
+  <div class="col-md-6">
+    <h1>Tambah Project</h1>
+    <form action="" method="post">
+      <div class="form-group">
+        <label>NAMA</label>
+        <input type="text" name="nama" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>UPLOAD FOTO</label>
+        <input type="file" name="foto" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>KETERANGAN</label>
+        <textarea name="keterangan" cols="30" rows="3" class="form-control"></textarea>
+      </div>
+      <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+    </form>
+  </div>
+</div>
